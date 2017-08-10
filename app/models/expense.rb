6f6b6e9d6d7f424943fb0d222b31939d59f76670
cristian-castrillon <cristian.castrillon@gmail.com>
2017-08-10 15:40:54 -0500

    [ADD] Annotations to models with annotate

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id           :integer          not null, primary key
+#  expense_type :string
+#  date         :date
+#  concept      :string
+#  category_id  :integer
+#  amount       :decimal(, )
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Expense < ApplicationRecord
   belongs_to :category
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id           :integer          not null, primary key
+#  expense_type :string
+#  date         :date
+#  concept      :string
+#  category_id  :integer
+#  amount       :decimal(, )
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
